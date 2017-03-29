c5f8128b8f2481bad300e57283c627b99c303c95
cozrica <rika.okoshi@gmail.com>
2017-03-29 14:24:41 +0900

    Add a micropost model

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -47,9 +47,8 @@ class PasswordResetsController < ApplicationController
     end
 
     def valid_user
-      unless (@user && @user.activated? &&
-          @user.authenticated?(:reset, params[:id]))
-        redirect_to root_url
+      unless (@user&.authenticated?(:reset, params[:id]) && @user.activated?)
+          redirect_to root_url
       end
     end
 
