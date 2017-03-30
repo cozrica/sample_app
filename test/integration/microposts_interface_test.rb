7aee1ecb36791dd14082e10dfc0d9102945530ce
cozrica <rika.okoshi@gmail.com>
2017-03-30 10:58:52 +0900

    Upload a picture on a micropost

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -10,6 +10,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type=submit]'
     # 無効なマイクロポスト投稿
     assert_no_difference 'Micropost.count' do
       post microposts_path, params: { micropost: { content: "" } }
@@ -17,11 +18,15 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     # 有効な投稿
     content = "Valid post"
+    picture = fixture_file_upload('test/fixtures/kitten.jpg', 'image/jpg')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, params: { micropost: { content: content } }
+      post microposts_path, params: { micropost: { content: content,
+                                                   picture: picture } }
     end
+    assert assigns(:micropost).picture?
     assert_redirected_to root_path
     follow_redirect!
+    assert_match content, response.body
     # 投稿を削除する
     assert_select 'a', text: 'delete'
     micropost = @user.microposts.paginate(page: 1).first
