20fd9a7777a2fdace47a953473a8ab19be129cec
cozrica <rika.okoshi@gmail.com>
2017-03-30 15:13:41 +0900

    Add a relationship model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -85,4 +85,15 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
+  test "should follow and unfollow a user" do
+    user = users(:michael)
+    other_user = users(:mina)
+    assert_not user.following?(other_user)
+    user.follow(other_user)
+    assert user.following?(other_user)
+    assert other_user.followers.include?(user)
+    user.unfollow(other_user)
+    assert_not user.following?(other_user)
+  end
+
 end
